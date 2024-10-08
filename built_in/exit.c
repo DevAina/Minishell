289ed9340980daee60c2a23adf0b96b45baa8f2d
Tanjona010 <tanjonarav@gmail.com>
2024-10-08 12:38:30 +0300

    fix exit

--- built_in/exit.c
+++ built_in/exit.c
@@ -6,7 +6,7 @@
 /*   By: trarijam <trarijam@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 10:17:50 by traveloa          #+#    #+#             */
-/*   Updated: 2024/09/28 07:37:33 by traveloa         ###   ########.fr       */
+/*   Updated: 2024/10/08 11:04:42 by traveloa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,27 @@ void	get_exit_status(int *status, char **cmd)
 	}
 }
 
+static int		check_digit(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static void	exit_n_free(char **envp, t_ast_node *ast, int status)
+{
+	free_ast(&ast);
+	free_split(envp);
+	exit(status);
+}
+
 int	ft_exit(char **cmd, t_ast_node *ast, char **envp, int flag)
 {
 	int	status;
@@ -39,18 +60,17 @@ int	ft_exit(char **cmd, t_ast_node *ast, char **envp, int flag)
 	if (cmd[1] && cmd[2])
 	{
 		ft_putstr_fd(" too many arguments\n", 2);
-		return (EXIT_FAILURE);
+		if (check_digit(cmd[1]) == 1)
+			return (EXIT_FAILURE);
+		else
+			exit_n_free(envp, ast, 2);
 	}
-	if (cmd[1])
+	else if (cmd[1])
 	{
 		status = ft_atoi(cmd[1]);
 		get_exit_status(&status, cmd);
 	}
 	if (flag == 1)
-	{
-		free_ast(&ast);
-		free_split(envp);
-		exit(status);
-	}
+		exit_n_free(envp, ast, status);
 	return (status);
 }
