277d96552e36f9e0eeac618fda6480ba967e896d
Tanjona010 <tanjonarav@gmail.com>
2024-10-18 14:12:32 +0300

    fix pwd return status

--- built_in/pwd.c
+++ built_in/pwd.c
@@ -6,7 +6,7 @@
 /*   By: trarijam <trarijam@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 08:11:34 by traveloa          #+#    #+#             */
-/*   Updated: 2024/10/16 08:26:09 by traveloa         ###   ########.fr       */
+/*   Updated: 2024/10/18 14:08:42 by traveloa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,8 @@ int	ft_pwd(char **args, char **env)
 	(void)env;
 	pwd = NULL;
 	pwd = getcwd(NULL, 0);
-	if (pwd)
-	{
-		ft_putendl_fd(pwd, 1);
+	ft_putendl_fd(pwd, 1);
+	if (pwd)	
 		free(pwd);
-	}
-	else
-	{
-		pwd = get_env_value("PWD", env);
-		ft_putendl_fd(pwd, 1);
-	}
-	return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
